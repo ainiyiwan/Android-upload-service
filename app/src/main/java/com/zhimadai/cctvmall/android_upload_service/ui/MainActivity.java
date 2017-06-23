f0bb11b0f2b78a8b3d2729d4dbcd6c5dd1781d5e
Jack <ainiyiwanxxl@gmail.com>
2017-06-23 14:07:31 +0800

    Initial commit

---
+++ app/src/main/java/com/zhimadai/cctvmall/android_upload_service/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zhimadai.cctvmall.android_upload_service.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhimadai.cctvmall.android_upload_service.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
